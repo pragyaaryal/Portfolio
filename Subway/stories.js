ab6a50af32c22b5383cda646244e2be8ef37ecba
pragyaaryal <pragyaaryal18@gmail.com>
2026-05-07 16:33:20 -0400

    Hosting Subway diaries

---
+++ Subway/stories.js
@@ -0,0 +1,82 @@
+window.STORIES = [
+  {
+    id: 1,
+    title: "Smth smth",
+    stationName: "14th St - Union Square",
+    lat: 40.7349,
+    lng: -73.9901,
+    text: "bla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabl bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 2,
+    title: "bhai",
+    stationName: "96th St (Upper West Side)",
+    lat: 40.7939,
+    lng: -73.9723,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 3,
+    title: "First ride",
+    stationName: "Times Sq - 42nd St (Manhattan)",
+    lat: 40.7550,
+    lng: -73.9867,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 4,
+    title: "Showing my world",
+    stationName: "Atlantic Ave - Barclays Center (Brooklyn)",
+    lat: 40.6845,
+    lng: -73.9772,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 5,
+    title: "Showing my world",
+    stationName: "Jackson Heights - Roosevelt Ave (Queens)",
+    lat: 40.7465,
+    lng: -73.8910,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 6,
+    title: "Showing my world",
+    stationName: "Yankee Stadium - 161st St (Bronx)",
+    lat: 40.8279,
+    lng: -73.9257,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 7,
+    title: "Showing my world",
+    stationName: "Coney Island - Stillwell Ave (Brooklyn)",
+    lat: 40.5773,
+    lng: -73.9818,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  },
+  {
+    id: 8,
+    title: "Showing my world",
+    stationName: "96th St",
+    lat: 40.7939,
+    lng: -73.9723,
+    text: "bla bla blaaaa",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../images/trial.jpg"
+  }
+];
