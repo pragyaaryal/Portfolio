6a3c3def089bf3c2bd1062663bdf7125e017b0e8
pragyaaryal <pragyaaryal18@gmail.com>
2026-05-21 09:25:25 -0400

    Added essays

--- Subway/stories.js
+++ Subway/stories.js
@@ -1,82 +1,86 @@
 window.STORIES = [
   {
     id: 1,
-    title: "Smth smth",
-    stationName: "14th St - Union Square",
-    lat: 40.7349,
-    lng: -73.9901,
-    text: "bla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabl bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaabla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/1.png"
+    title: "My Introduction to Public Space",
+    stationName: "Lexington Avenue/51st Street",
+    lat: 40.7570, 
+    lng: -73.9719,
+  
+    text: "As a part of my CityGap application in May of 2025, exactly a year ago, Tanya and Andy asked me to build a neighbourhood biography that included 3 meaningful spaces in my neighbourhood...",
+
+    readMoreLink: "./essays/intro.html",
+  
+    date: "Sept 12, 2025 • 4:52 PM",
+  
+    image: "../Subway/images/42.png"
   },
+ 
   {
     id: 2,
-    title: "bhai",
-    stationName: "96th St (Upper West Side)",
-    lat: 40.7939,
-    lng: -73.9723,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
+    title: "My Favorite “Public Space” – the Subway",
+    stationName: "Brooklyn Bridge–City Hall Station",
+    lat: 40.7077,
+    lng: -74.0028,
+    text: "After studying public spaces,  some in the South Bronx as part of our group project with the Department of Transportation last semester, and writing a 32-page comparative analysis on public spaces in NYC and Kathmandu, I would like to call myself sort of an expert...",
+    readMoreLink: "./essays/fav.html",
+    date: "Sept 19, 2025 • 9:36 AM",
     image: "../Subway/images/2.png"
   },
+  
   {
     id: 3,
-    title: "First ride",
-    stationName: "Times Sq - 42nd St (Manhattan)",
-    lat: 40.7550,
-    lng: -73.9867,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/3.png"
-  },
-  {
-    id: 4,
-    title: "Showing my world",
-    stationName: "Atlantic Ave - Barclays Center (Brooklyn)",
-    lat: 40.6845,
-    lng: -73.9772,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/4.png"
-  },
-  {
-    id: 5,
-    title: "Showing my world",
-    stationName: "Jackson Heights - Roosevelt Ave (Queens)",
-    lat: 40.7465,
-    lng: -73.8910,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/5.png"
-  },
-  {
-    id: 6,
-    title: "Showing my world",
-    stationName: "Yankee Stadium - 161st St (Bronx)",
-    lat: 40.8279,
-    lng: -73.9257,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/trial.jpg"
-  },
-  {
-    id: 7,
-    title: "Showing my world",
+    title: "My First Subway Ride – More than Just Directions",
     stationName: "Coney Island - Stillwell Ave (Brooklyn)",
     lat: 40.5773,
     lng: -73.9818,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/trial.jpg"
+    text: "Piss and Weed, that was my first impression of the New York City Subway. My first subway ride was on September 1, 2025 when I came here with my aunt’s family from Long Island to go to Coney Island. I didn't even realize when we had switched from the Long Island Rail Road (LIRR) to the underground subway station.",
+    readMoreLink: "./essays/fav.html",
+    date: "Sept 1, 2025 • 13:56 PM",
+    image: "../Subway/images/5.png"
   },
   {
-    id: 8,
-    title: "Showing my world",
+    id: 4,
+    title: "My Journey to the Express Train",
+    stationName: "Van Cortlandt Park - 242 St",
+    lat: 40.8900, 
+    lng: -73.8981,
+    text: "I think I have spent a quarter of my time in New York City on the subway. When I first came to the city, for the CityGAP Program with Living City Project, I liked the 6 local trains.",
+    readMoreLink: "./essays/express.html",
+    date: "Oct 12, 2024 • 11:45 PM",
+    image: "../Subway/images/44.png"
+  },
+  {
+    id: 5,
+    title: "Subway Etiquette – Insider’s Rules to Navigate the Underground City",
+    stationName: "Times Sq - 42nd St (Manhattan)",
+    lat: 40.7550,
+    lng: -73.9867,
+    text: "One of the many reasons I like New York is because of its people, who are infamous for being rude. In reality, New Yorkers are the kindest people I have ever met; however, they are also the most annoyed people, and rightfully so",
+    readMoreLink: "./essays/express.html",
+    date: "December 3, 2025 • 17:14 PM",
+    image: "../Subway/images/41.png"
+  },
+  {
+    id: 6,
+    title: "Invisibility- Do I See Them or Do I Make Them Invisible?",
     stationName: "96th St",
     lat: 40.7939,
     lng: -73.9723,
-    text: "bla bla blaaaa",
-    date: "Oct 12, 2024 • 11:45 PM",
-    image: "../Subway/images/trial.jpg"
-  }
+    text: "I always receive a call from my aunt at nine in the morning, but it’s really my four-year-old cousin on FaceTime. He is always so excited to see me. The first thing he asks is when I’m leaving for work and heading out of the house. The excitement is not really for me…",
+    readMoreLink: "./essays/invisibility.html",
+    date: "April 28, 2026 • 9:36 AM",
+    image: "../Subway/images/9.png"
+  },
+  {
+    id: 7,
+    title: "Under the Banner of the NewYork City Subway",
+    stationName: "Jackson Heights–Roosevelt Avenue",
+    lat: 40.74676, 
+    lng: -73.8915,
+    text: "I remember reading an essay early on in our program which spoke about how New Yorkers move fast and with intention from one place to another, but they also come together when needed before dispersing just as quickly because “that’s what New Yorkers do.”",
+    readMoreLink: "./essays/under.html",
+    date: "Nov 14, 2025 • 4:22 PM",
+    image: "../Subway/images/43.png"
+  },
+
 ];
